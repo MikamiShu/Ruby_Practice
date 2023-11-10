5a71265bb50852ee4bedd4d1dca6806088a454bc
MIKAMI Shu <114971799+MikamiShu@users.noreply.github.com>
2023-11-10 12:44:19 +0900

    add test3

---
+++ Rails_test/test3/app/controllers/test_controller.rb
@@ -0,0 +1,24 @@
+class TestController < ApplicationController
+
+    def index
+    end
+
+    def update
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def destroy
+    end
+    
+end
