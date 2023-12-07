0c323653431964317c5aad2d5aa21c2a112da861
MIKAMI Shu <114971799+MikamiShu@users.noreply.github.com>
2023-12-07 13:55:19 +0900

    new delete OK!!!!

--- Rails_test/test4/app/controllers/users_controller.rb
+++ Rails_test/test4/app/controllers/users_controller.rb
@@ -8,6 +8,26 @@ class UsersController < ApplicationController
         @user = User.new
     end
     
+    def create 
+        @user = User.new(user_params)
+        if @user.save 
+            redirect_to users_path
+        else
+            render :new
+        end
+    end
+
+    def destroy 
+        @user = User.find(params[:id])
+        @user.destroy
+        redirect_to users_path
+    end
+
+    @private
+
+    def user_params 
+        params.require(:user).permit(:text_content, :sort_word)
+    end
 
 
     
