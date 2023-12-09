e9cf4c2a492bb6f2936f773ad9a6b79cc1a73456
MIKAMI Shu <114971799+MikamiShu@users.noreply.github.com>
2023-12-09 18:48:53 +0900

    mv document directory

--- Rails_test/test4/app/controllers/sessions_controller.rb
+++ Rails_test/test4/app/controllers/sessions_controller.rb
@@ -1,5 +1,16 @@
 class SessionsController < ApplicationController
     def new 
     end
+
+    def create 
+        user = User.find_by(email: params[:session][:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+            log_in user
+            redirect_to root_url
+        else
+            render 'new'
+        end
+    end
+    
     
 end
--- Rails_test/test4/config/routes.rb
+++ Rails_test/test4/config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
-  #get '/login' to: 'sessions#new'
-  #post '/login' to: 'sessions#create'
+  get '/login' to: 'sessions#new'
+  post '/login' to: 'sessions#create'
 
 
   resources :users
